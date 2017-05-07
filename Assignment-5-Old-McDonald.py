e2cb0dcb98e340941ac41f2bfb9b407cdd6052bb
dan <dan@callpipe.com>
2017-05-06 17:06:40 -0700

    Finished minimal version!

--- Assignment-5-Old-McDonald.py
+++ Assignment-5-Old-McDonald.py
@@ -3,13 +3,24 @@
 # Challenge
 # This script calculates statistics on sentences that were inputted by the user.
 
-animal = "wild bullshit"
-sound = "noisy af"
+
+def title():
+    print("Old McDonald\n")
 
 
-def base():
+def verse(animal, sound):
     print("Old McDonald had a farm, E-I-E-I-O.")
     print("And on that farm he had a {}, E-I-E-I-O.".format(animal))
     print("With a {sound}-{sound} here, and a {sound}-{sound} there.".format(sound=sound))
     print("Here a {sound}, there a {sound}, everywhere a {sound}-{sound}.".format(sound=sound))
-    print("Old McDonald had a farm, E-I-E-I-O.")
+    print("Old McDonald had a farm, E-I-E-I-O.\n")
+
+
+def main():
+    title()
+    verse('chicken', 'cluck')
+    verse('cow', 'moo')
+    verse('duck', 'quack')
+
+
+main()
