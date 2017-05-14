ac6af4acb65b10639b2191456d71d22ffb299d85
dan <dan@callpipe.com>
2017-05-14 16:25:23 -0700

    Mostly done with Assignment 6\

--- Assignment-6-There-was-an-Old-Lady.py
+++ Assignment-6-There-was-an-Old-Lady.py
@@ -13,6 +13,10 @@ def swallowed(animal, punctuation):
     print ("There was an old lady who swallowed a {}{}".format(animal, punctuation))
 
 
+def swallowedCatch(animal, animal2, punctuation):
+    print("She swallowed the {} to catch the {}{}".format(animal, animal2, punctuation))
+
+
 def wriggled():
     print("That wriggled and jiggled and tickled inside her.")
     print("She swallowed the spider to catch the fly.")
@@ -21,11 +25,6 @@ def wriggled():
 def dontKnowWhy():
     print("I don't know why she swallowed the fly.")
     print("Perhaps she'll die.\n")
-
-
-def end():
-    print("There was an old lady who swallowed a horse.")
-    print("She's dead, of course.")
 
 
 def verse1():
@@ -41,8 +40,44 @@ def verse2():
 
 def verse3():
     swallowed("bird", ",")
+    print("How absurd to swallow a bird.")
+    swallowedCatch("bird", "spider", ",")
     wriggled()
     dontKnowWhy()
+
+
+def verse4():
+    swallowed("cat", ".")
+    print("Imagine that to swallow a cat.")
+    swallowedCatch("cat", "bird", ".")
+    swallowedCatch("bird", "spider", ",")
+    wriggled()
+    dontKnowWhy()
+
+
+def verse5():
+    swallowed("dog", ".")
+    print("My, what a hog, to swallow a dog.")
+    swallowedCatch("dog", "cat", ".")
+    swallowedCatch("cat", "bird", ".")
+    swallowedCatch("bird", "spider", ",")
+    wriggled()
+    dontKnowWhy()
+
+def verse6():
+    swallowed("hawk", ".")
+    print("Would you try to swallow a hawk?")
+    swallowedCatch("hawk", "dog", ".")
+    swallowedCatch("dog", "cat", ".")
+    swallowedCatch("cat", "bird", ".")
+    swallowedCatch("bird", "spider", ",")
+    wriggled()
+    dontKnowWhy()
+
+
+def lastverse():
+    swallowed("horse", ".")
+    print("She's dead, of course.")
 
 
 def main():
@@ -65,11 +100,11 @@ def main():
     # dog verse
     verse5()
 
-    # horse verse
-    lastverse()
-
     # new verse
     verse6()
+
+    # horse verse
+    lastverse()
 
 
 # call main
