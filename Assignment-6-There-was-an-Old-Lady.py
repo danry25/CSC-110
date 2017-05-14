169a699b19a92da8f5543b20170aa5491c027fc0
dan <dan@callpipe.com>
2017-05-14 13:03:57 -0700

    Started Assignment 6

---
+++ Assignment-6-There-was-an-Old-Lady.py
@@ -0,0 +1,32 @@
+# Dan Ryan
+# Assignment 5: Old McDonald
+# Challenge
+#
+
+
+def main():
+
+    # fly verse
+    verse1()
+
+    # spider verse
+    verse2()
+
+    # bird verse
+    verse3()
+
+    # cat verse
+    verse4()
+
+    # dog verse
+    verse5()
+
+    # horse verse
+    lastverse()
+
+    # new verse
+    verse6()
+
+
+# call main
+main()
