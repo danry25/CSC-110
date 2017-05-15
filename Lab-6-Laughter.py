cf0d5e52ab06d14ff6a4a7888e97426422559eb3
dan <dan@callpipe.com>
2017-05-14 17:36:42 -0700

    Commented my work on Lab 6

--- Lab-6-Laughter.py
+++ Lab-6-Laughter.py
@@ -20,31 +20,30 @@ def main():
     else:
         if nameretry <= 2:
             pass
+        # Give up on user entering a good name
         else:
             print("You've tried entering a name 3 times, we're giving up & setting a default value")
             name = "Krampus"
-    # while not rating.isdigit():
-    #     # user input error for rating
-    #     print('Error in input. Rating must be a non-negative integer.')
-    #     rating = input('Try again. Enter the evilness rating: ')
-    # Convert rating from string to int
-    # rating = int(rating)
     # Generate the laugh to be printed, then print it
     while ratingretry:
+        # Convert rating to int
         rating2 = int(rating)
         if rating2 == 0:
             laugh = 'Hee hee'
             ratingretry = False
+        # Slice and compare rating to deal with negative values
         elif rating[0] == '-' and rating2 <= 7 and rating2 >= -7 and len(rating) == 2:
             laugh = 'Ho' * int(rating[1])
             ratingretry = False
         elif rating2 <= 7 and rating2 >= -7:
             laugh = 'Bwa' + 'ha' * rating2
             ratingretry = False
+        # Ask for another go at input, as it was incorrect before
         else:
             rating = input('Try again. Enter the evilness rating: ')
             ratingretry = True
 
+    # Return string to user!
     print('My name is %s. %s.' % (name, laugh))
 
 
