3019302158a411f38334211daf1ec9fcc914c58d
dan <dan@callpipe.com>
2017-05-21 14:59:10 -0700

    Slowly making headway.

--- Assignment-7-Tic-Tac-Toe.py
+++ Assignment-7-Tic-Tac-Toe.py
@@ -15,8 +15,12 @@ win.title('Tic-Tac-Toe')
 # make the canvas
 canvas = win.ca(CANVAS_WIDTH, CANVAS_HEIGHT)
 
+positions = ["UPPER_LEFT", "UPPER_MIDDLE", "UPPER_RIGHT", "MID_LEFT", "CENTER", "MID_RIGHT",
+             "LOWER_LEFT", "LOWER_MIDDLE", "LOWER_RIGHT"]
+squares = ["[0, 0]", "[100, 100]"]
 
-def grid():
+
+def board(x, y, canvas):
 
     # make upper horizontal line
     canvas.rectangle([[-150, 50],
@@ -37,6 +41,19 @@ def grid():
     # upTriangle(-100, -50, 120, 30, '#993300', canvas)
 
 
+# helper function to draw X
+def drawX():
+    pass
+
+
+# helper function to draw O
+def drawO(position, canvas):
+    coord = [100, 100]
+    # squares[positions.index(position)]
+    canvas.circle(coord, 40, width=7)
+    pass
+
+
 # define a helper function to draw a triangle
 def upTriangle(x, y, w, h, color, canvas):
     canvas.polygon([[x, y], [x + w, y], [x + w/2, y + h]],
@@ -44,7 +61,8 @@ def upTriangle(x, y, w, h, color, canvas):
 
 
 # call main, which creates the Tic-Tac-Toe grid/board
-grid()
+board(-150, -150, canvas)
+drawO("UPPER_LEFT", canvas)
 
 # invoke tkinter: displays objects drawn!
 win.mainloop()
