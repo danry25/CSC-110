1c68ff491f6ad9f9bf8b8a1eb9eda9dc5b9e0dc3
dan <dan@callpipe.com>
2017-05-24 16:40:06 -0700

    Ugh, its not working :c

--- Lab-8-Jabber.py
+++ Lab-8-Jabber.py
@@ -4,13 +4,36 @@
 # Find some words containing a string in a file
 
 # Bring in sys.argv to handle runtime options
-import sys.argv as userarg
+# import sys.argv as userarg
 
 # Open, read & parse file into an array so we can work with it
 jf = open('jabber.txt', 'r')
+# Open jabber-selected-words.txt for writing
+jswf = open('jabber-selected-words.txt', 'w')
 
 # Loop through each item in array
 for word in jf:
     # Check if there are any duplicates in the array
-    if word in jf():
+    if word in jf:
         pass
+
+
+# read a line from jabber.txt
+line = jf.readline()
+
+# while there are still lines to be read:
+while line != '':
+    print("holla")
+    # check if the line contains a 'j':
+    if 'j' in line:
+        # if it does, write it to jabber-selected-words.txt
+        jswf.append(line)
+        print(line)
+    # read the next line from jabber.txt
+    line = jf.readline()
+
+# close the files
+jf.close()
+jswf.close()
+
+print(line)
