a6a34fbe3b322a65a657f52f66d5c447c251a7d0
dan <dan@callpipe.com>
2017-05-24 18:44:57 -0700

    Commented my file

--- Lab-8-Jabber.py
+++ Lab-8-Jabber.py
@@ -1,7 +1,7 @@
 # Dan Ryan
 # Lab 8: Jabber
 # Plus
-# Find some words containing a string in a file
+# Find some words containing a string or a letter in a file
 
 # Bring in sys.argv to handle runtime options
 import sys as sys
@@ -9,27 +9,38 @@ import sys as sys
 # Open, read & parse file into an array so we can work with it
 all_words = []
 letter = ''
+# Open file
 with open('jabber.txt') as j:
+    # Process line into variable
     line = j.readline()
+    # Loop through each line and process it into all_words
     while line != "":
         all_words += line.strip().split(" ")
         line = j.readline()
 
 
+# Function for finding a letter or string in a word
 def letterProcessor():
-            letter_words = []  # All the words that have this letter in them
+            # All the words that have this letter in them
+            letter_words = []
+            # Loop through each word
             for word in all_words:
+                # Append these words to the array if they match
                 if letter in word.lower():
                     letter_words.append(word)
+            # Output all these words into a labeled set
             out.write("{} words:\n".format(letter.upper()))
             out.write(" ".join(letter_words))
             out.write("\n")
 
 
+# Write the labeled set all to a file
 with open('jabber-selected-words.txt', 'w') as out:
+    # Check if any arguments were passed when running the file
     try:
         letter = sys.argv[1]
         letterProcessor()
+    # If not, just do the whole alphabet
     except:
         for letter in 'abcdefghijklmnopqrstuvwxyz':
             letter = letter
