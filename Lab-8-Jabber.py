828bca60e0159f584cf69e97fd987892b2c118b6
dan <dan@callpipe.com>
2017-05-24 17:54:18 -0700

    Broken Lab 8

--- Lab-8-Jabber.py
+++ Lab-8-Jabber.py
@@ -18,24 +18,31 @@ chars = 'j'
 
 # While there are still lines to be read:
 while line != '':
-    # Check if the line contains a j or J:
-    if chars in line:
-        # if it does, write it to jabber-selected-words.txt
-        jswf.write(line)
-        # Then strip the newline bit
-        line = line.strip('\n')
-        # Append it to the array to be printed
+    if line not in jsw:
         jsw.append(line)
-    elif 'J' in line and chars == 'j':
-        # if it does, write it to jabber-selected-words.txt
-        jswf.write(line)
-        # Then strip the newline bit
-        line = line.strip('\n')
-        # Append it to the array to be printed
-        jsw.append(line)
-    # read the next line from jabber.txt
-    line = jf.readline()
+            print(line, end=' ')
+        # read the next line from jabber.txt
+        line = jf.readline()
 
+
+# Check if the line contains a letter:
+for letter in 'abcdefghijklmnopqrstuvwxyz':
+    chars = letter
+    if chars in jsw:
+        # if it does, write it to jabber-selected-words.txt
+        jswf.write(line)
+        # Then strip the newline bit
+        line = line.strip('\n')
+        # Append it to the array to be printed
+        jsw.append(line)
+        print(line, end=' ')
+    elif chars.upper() in line:
+        # if it does, write it to jabber-selected-words.txt
+        jswf.write(line)
+        # Then strip the newline bit
+        line = line.strip('\n')
+        # Append it to the array to be printed
+        jsw.append(line)
 
 # close the files
 jf.close()
