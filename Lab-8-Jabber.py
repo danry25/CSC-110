70c9665ee3b0a31fd0a53939937c95993b945219
dan <dan@callpipe.com>
2017-05-24 18:39:12 -0700

    Moved to functions, and completed plus version I think

--- Lab-8-Jabber.py
+++ Lab-8-Jabber.py
@@ -8,24 +8,15 @@ import sys as sys
 
 # Open, read & parse file into an array so we can work with it
 all_words = []
+letter = ''
 with open('jabber.txt') as j:
     line = j.readline()
     while line != "":
         all_words += line.strip().split(" ")
         line = j.readline()
 
-with open('jabber-selected-words.txt', 'w') as out:
-    try:
-        letter = sys.argv[1]
-        letter_words = []  # All the words that have this letter in them
-        for word in all_words:
-            if letter in word.lower():
-                letter_words.append(word)
-        out.write("{} words:\n".format(letter.upper()))
-        out.write(" ".join(letter_words))
-        out.write("\n")
-    except:
-        for letter in 'abcdefghijklmnopqrstuvwxyz':
+
+def letterProcessor():
             letter_words = []  # All the words that have this letter in them
             for word in all_words:
                 if letter in word.lower():
@@ -33,3 +24,13 @@ with open('jabber-selected-words.txt', 'w') as out:
             out.write("{} words:\n".format(letter.upper()))
             out.write(" ".join(letter_words))
             out.write("\n")
+
+
+with open('jabber-selected-words.txt', 'w') as out:
+    try:
+        letter = sys.argv[1]
+        letterProcessor()
+    except:
+        for letter in 'abcdefghijklmnopqrstuvwxyz':
+            letter = letter
+            letterProcessor()
