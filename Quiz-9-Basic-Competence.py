d828cf94b49efae297f26cf62a9b988448f872ba
dan <dan@callpipe.com>
2017-06-04 16:43:18 -0700

    Finished quiz

--- Quiz-9-Basic-Competence.py
+++ Quiz-9-Basic-Competence.py
@@ -1,29 +1,32 @@
 
 
-def twentyval():
+# Sums odd values in array
+def helper(twenty):
+    # Running total
+    total = 0
+    for num in twenty:
+        # If the number is odd,  this is triggered
+        if not num % 2 == 0:
+            # Add current number to total
+            total += num
+    # Throw sum back to main
+    return total
+
+
+# Main function which calls all the helper functions
+def main():
     # declare an array that can hold 20 integer values
     storage = []
     # use a loop to put the integer values 1, 2, 3, ..., 18, 19, 20 into the array
     for num in range(20):
+        # appends to array here, adding 1 to get correct values
         storage.append(num + 1)
-    return storage
-
-
-def helper(twenty):
-    total = 0
-    print(twenty)
-    for num in twenty:
-        if not num % 2 == 0:
-            print(num)
-            total += num
-
-
-def main():
-    twenty = twentyval()
+        # Throws data back to main for later use
     # pass the array to the helper function which will return an integer value
-    total = helper(twenty)
+    total = helper(storage)
     # print out the value returned by the helper function
     print(total)
 
 
+# Call main so it will actually be ran :P
 main()
