47c5b3766545e413f17e91599d297bf3dc45f8a5
dan <dan@callpipe.com>
2017-06-04 15:43:33 -0700

    Redid with functions

--- Assignment-9-Presidents.py
+++ Assignment-9-Presidents.py
@@ -2,11 +2,11 @@
 # Assignment 9: Presidents
 # Challenge
 # Parse text file and create new files with data ordered by date, name, etc.
-pres = []
 
 
 def fileParser(inFile):
-    # try:
+    pres = []
+    try:
         # Open and parse requested file
         unparsedFile = open(inFile)
         # Process line into variable
@@ -26,20 +26,28 @@ def fileParser(inFile):
             # Read next line
             line = unparsedFile.readline()
         unparsedFile.close()
-    # except:
-    #     # If input file doesn't exist, complain and exit
-    #     print('Input file {} could not be read :c'.format(inFile))
+        return pres
+    except:
+        # If input file doesn't exist, complain and exit
+        print('Input file {} could not be read :c'.format(inFile))
 
 
-def sameOrder():
+def sameOrder(pres):
+    meter = pres[::3]
+    counter = 0
+    data = []
+    for thing in meter:
+        data.append(thing + ' was president from ' + pres[1+counter] + ' to ' + pres[2+counter] + '.' '\n')
+        counter += 3
+    return data
+
+
+def fileWriter(outFile, data):
     try:
         # Write parsed list to disk
-        parsedFile = open('same_order.txt', 'w')
-        meter = pres[::3]
-        counter = 0
-        for thing in meter:
-            parsedFile.write(thing + ' was president from ' + pres[1+counter] + ' to ' + pres[2+counter] + '.' '\n')
-            counter += 3
+        parsedFile = open(outFile, 'w')
+        for thing in data:
+            parsedFile.write(thing)
         # Close files
         parsedFile.close()
     except:
@@ -48,9 +56,9 @@ def sameOrder():
 
 
 def main():
-    fileParser('presidents.txt')
-    sameOrder()
-    print(pres)
+    pres = fileParser('presidents.txt')
+    same = sameOrder(pres)
+    fileWriter('same_order.txt', same)
 
 
 main()
