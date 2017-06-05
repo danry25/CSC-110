302df924f5f66a4de566083f62cd89164bb6026a
dan <dan@callpipe.com>
2017-06-04 17:22:44 -0700

    Wrote report, finished challenge version

--- Assignment-9-Presidents.py
+++ Assignment-9-Presidents.py
@@ -1,9 +1,13 @@
 # Dan Ryan
 # Assignment 9: Presidents
 # Challenge
-# Parse text file and create new files with data ordered by date, name, etc.
+# Parse text file and create new files with data ordered by date, name and inauguration.
+
+# Nab datetime so we can deal with super specific time/date info
+from datetime import datetime
 
 
+# Parses files based on input when called
 def fileParser(inFile):
     pres = []
     try:
@@ -13,17 +17,15 @@ def fileParser(inFile):
         line = unparsedFile.readline()
         # Loop through each line and process it into separatedWords
         while line != "":
+            # Split each line up into usable chunks
             lineParsed = line.strip().split("\t")
-            if not len(lineParsed) == 5:
-                pres.append(lineParsed[1] + ' ' + lineParsed[0])
-                pres.append(lineParsed[2])
-                pres.append(lineParsed[3])
-                pres.append(lineParsed[0])
-            else:
-                pres.append(lineParsed[2] + ' ' + lineParsed[1] + ' ' + lineParsed[0])
-                pres.append(lineParsed[3])
-                pres.append(lineParsed[4])
-                pres.append(lineParsed[0])
+            # Append it to pres so we can use it later...
+            pres.append({
+                "firstName": lineParsed[1],
+                "lastName": lineParsed[0],
+                "start": lineParsed[2],
+                "end": lineParsed[3]
+            })
 
             # Read next line
             line = unparsedFile.readline()
@@ -34,29 +36,30 @@ def fileParser(inFile):
         print('Input file {} could not be read :c'.format(inFile))
 
 
-def sameOrder(pres):
-    meter = pres[::4]
-    counter = 0
-    data = []
-    for thing in meter:
-        data.append(thing + ' was president from ' + pres[1+counter] + ' to ' + pres[2+counter] + '.' '\n')
-        counter += 4
-    return data
+# Formats the sentence and returns it to main
+def writeSentence(president):
+    return "{firstName} {lastName} was president from {start} to {end}\n".format(**president)
 
 
-def lastName(pres):
-    meter = pres[::4]
-    counter = 0
-    orgData = []
-    data = []
-    orgData = sorted(pres, key=lambda president: president[3])
-    print(orgData)
-    for thing in meter:
-        data.append(orgData[counter] + ' was president from ' + orgData[1+counter] + ' to ' + orgData[2+counter] + '.' '\n')
-        counter += 4
-    return data
+# Creates file with same internal list order
+def sameOrder(presidents):
+    # Fun inline for loops are fun & unconventional :P
+    return [writeSentence(p) for p in presidents]
 
 
+# Sorts by last name
+def lastName(presidents):
+    # inline for loop utilizing sorted to sort alphabetically by last name
+    return [writeSentence(p) for p in sorted(presidents, key=lambda president: president['lastName'])]
+
+
+# Sorts by exact inauguration time
+def inauguration(presidents):
+    # Here I get to use the same for loop and sorted function to sort by date :P Feed me an exact time plz for funziez!
+    return [writeSentence(p) for p in sorted(presidents, key=lambda p: datetime.strptime(p['start'], '%m/%d/%Y'))]
+
+
+# Writes files to disk
 def fileWriter(outFile, data):
     try:
         # Write parsed list to disk
@@ -70,23 +73,21 @@ def fileWriter(outFile, data):
         print('Could not open or write to {}, check your file permissions.'.format('same_order.txt'))
 
 
-class Student:
-    def __init__(self, name, grade, age):
-        self.name = name
-        self.grade = grade
-        self.age = age
-        
-    def __repr__(self):
-         return repr((self.name, self.grade, self.age))
-
-
+# Run the functions to process and create the files
 def main():
+    # Parse the input file
     pres = fileParser('presidents.txt')
+    # Output the file in the same order (but beautified)
     same = sameOrder(pres)
+    # Write all that good stuff to disk
     fileWriter('same_order.txt', same)
+    # Now we sort by last names, wheee!
     last = lastName(pres)
+    # Then pass that freshly sorted data to fileWriter and get it on disk
     fileWriter('last_name.txt', last)
-    # inauguration.txt
+    # Here I just decided to call my function inline, then write the output to disk. See, I can save space too!
+    fileWriter('inauguration.txt', inauguration(pres))
 
 
+# Run the main function
 main()
