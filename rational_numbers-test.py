85a9ce3122560fbea0bcc6352205436a77fe914c
dan <dan@callpipe.com>
2017-06-16 22:49:49 -0700

    Done with this quiz!

--- rational_numbers-test.py
+++ rational_numbers-test.py
@@ -6,8 +6,11 @@ def main():
     a = rational_numbers.rational(3, 4)
     b = rational_numbers.rational(1, 2)
 
-    print('a =', a)
-    print('b =', b)
+    print(a, '+', b, '=', a.plus(b))
+    print(a, '*', b, '=', a.star(b))
+
+    print(a, '=', a.toFloat())
+    print(b, '=', b.toFloat())
 
 
 main()
--- rational_numbers.py
+++ rational_numbers.py
@@ -1,7 +1,37 @@
+# Dan Ryan
+# Quiz 11: The rational Class
+# Complete w/Extra Credit
+#
+
+
+# Define Class
 class rational:
-    def __init__(self, p, q):
-        self.p = p
-        self.q = q
+    # Constructor that takes two int values as arguments
+    def __init__(self, p, q=1):
+        # Appropriate handling of negative Q values
+        if p < 0 and q < 0:
+            self.p = p * -1
+            self.q = q * -1
+        # Exception when Q is zero (extra-credit)
+        elif q == 0:
+            raise ZeroDivisionError('Denominator cannot be zero')
+            # raise ZeroDivisionError("Can't divide {} by zero".format(p))
+        # Handle normal Q values
+        else:
+            self.p = p
+            self.q = q
 
     def __str__(self):
         return str('(' + str(self.p) + '/' + str(self.q) + ')')
+
+    # Support addition
+    def plus(self, b):
+        return str(str((self.p * b.q) + (self.q * b.p)) + '/' + str(self.q * b.q))
+
+    # Support multiplication
+    def star(self, b):
+        return str(str(self.p * b.p) + '/' + str(self.q * b.q))
+
+    # Support converting a rational to a float value
+    def toFloat(self):
+        return self.p / self.q
